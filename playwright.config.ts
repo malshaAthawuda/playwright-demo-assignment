841bca7ab66bfca18ea7fe244af0f5a53cac9627
Tharuka Wickramaarachchi <tharukaedu@gmail.com>
2026-03-07 09:09:18 +0530

    Configure Playwright end-to-end testing and integrate with GitHub Actions CI.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+
+// Read from default ".env" file.
+dotenv.config();
 
 export default defineConfig({
   testDir: './tests',
   timeout: 60 * 1000,
   reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
   use: {
-    headless: false,
+    headless: !!process.env.CI,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
